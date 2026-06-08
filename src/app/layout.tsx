6f3360da6e81e6cf4decb44396b89bb354443c02
Z User <z@container>
2026-06-08 19:35:38 +0000

    Fix favicon: emerald green background circle so it's visible in browser tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export const metadata: Metadata = {
   authors: [{ name: "CMOR Protocol" }],
   icons: {
     icon: [
+      { url: "/favicon.svg", type: "image/svg+xml" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
     ],
